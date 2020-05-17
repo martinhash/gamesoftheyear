7d83ca96874e940c31d8403ec8def0c63592ebc6
Martin Barria Joost <martinjoostw@gmail.com>
2020-05-17 17:15:39 -0600

    feat results

--- src/app/components/graphics-horizontal/graphics-horizontal.component.ts
+++ src/app/components/graphics-horizontal/graphics-horizontal.component.ts
@@ -1,50 +1,62 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-graphics-horizontal',
-  templateUrl: './graphics-horizontal.component.html',
-  styleUrls: ['./graphics-horizontal.component.css']
+  selector: "app-graphics-horizontal",
+  templateUrl: "./graphics-horizontal.component.html",
+  styleUrls: ["./graphics-horizontal.component.css"],
 })
-export class GraphicsHorizontalComponent  {
-
+export class GraphicsHorizontalComponent {
   results: any[] = [
     {
-      "name": "Juego 1",
-      "value": 50
+      name: "Juego 1",
+      value: 50,
     },
     {
-      "name": "Juego 2",
-      "value": 12
+      name: "Juego 2",
+      value: 12,
     },
     {
-      "name": "Juego 3",
-      "value": 20
+      name: "Juego 3",
+      value: 20,
     },
     {
-      "name": "Juego 4",
-      "value": 6
-    }
+      name: "Juego 4",
+      value: 6,
+    },
   ];
 
   view: any[] = [700, 400];
 
   // options
-  showXAxis   = true;
-  showYAxis   = true;
-  gradient    = true;
-  showLegend  = true;
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
   showXAxisLabel = true;
-  xAxisLabel = 'Juegos';
+  xAxisLabel = "Juegos";
   showYAxisLabel = true;
-  yAxisLabel = 'Votos';
+  yAxisLabel = "Votos";
 
-  colorScheme = 'nightLights';
+  colorScheme = "nightLights";
 
-  constructor(){}
+  constructor() {
+
+    setInterval( () => {
+      console.log('tick');
+
+      const newResults = [...this.results];
+
+      for( let i in newResults ) {
+        newResults[i].value = Math.round( Math.random() * 500 )
+      }
+
+      this.results = [...newResults];
+
+    }, 1500 );
+  
+  }
 
   onSelect(event) {
     console.log(event);
   }
-
-
 }
